515152cb2986f1c35460548e94aebc9f543218fc
sonubhardwajj19 <sonubhardwajj19@gmail.com>
2026-08-07 15:48:33 +0530

    Added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+
+import "./index.css";
+import { Button } from "./components/ui/button";
+export function App() {
+  return (<>
+    <div>
+      <div className="flex bg-zinc-600 p-3">
+        <div className="flex gap-4">
+            <Button>C++</Button>
+            <Button>Javascript</Button>
+            <Button>Python</Button>
+            <Button className="bg-gray-200 text-black hover:text-red-500 ml-20">Submit</Button>
+        </div>
+      </div>
+    
+      <div className="flex h-screen w-screen">
+        <div className="bg-red-200 flex-1"></div>
+        <div className="bg-green-400 flex-1"></div>
+      </div>
+    </div>
+  </> 
+  );
+}
+
+export default App;
