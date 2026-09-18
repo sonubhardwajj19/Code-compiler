0a196ae7319e87ebfce45d199b57517be1a51ecc
sonubhardwajj19 <sonubhardwajj19@gmail.com>
2026-09-18 22:58:14 +0530

    Updated ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
 import "./index.css";
 import { Button } from "./components/ui/button";
+import { Footer } from "./components/ui/footer";
 import { useRef, useState } from "react";
 import axios from "axios";
+
 
 
 const BACKEND_URL = "http://localhost:3000";
@@ -25,61 +27,74 @@ export function App() {
   return (<>
     <div>
       <div className="flex bg-gray-900 p-3 justify-between">
-        <div className="flex gap-4">
-            <Button variant={selectedLanguage === "c++" ? "destructive" : "outline"} onClick={() => setSelectedLanguage("c++")}>C++</Button>
-            <Button variant={selectedLanguage ==="js" ? "destructive" : "outline"} onClick={()=>{setSelectedLanguage("js")}}>Javascript</Button>
-            <Button variant={selectedLanguage ==="py" ? "destructive" : "outline"} onClick={()=>setSelectedLanguage("py")}>Python</Button>
-          
-            <Button onClick={async()=>{
-                setStatus("Processing");
-                setOutput("");
-                   
-                const response = await axios.post(`${BACKEND_URL}/submit`, {
-                   "code": textRef.current?.value ,
-                   "language":selectedLanguage
-                })
-  
-                pollBackend(response.data.id);
-              
-            }} className="ml-30" >Submit</Button>
-        </div>
+          <div className="flex gap-4">
+              <Button variant={selectedLanguage === "c++" ? "destructive" : "outline"} onClick={() => setSelectedLanguage("c++")}>C++</Button>
+              <Button variant={selectedLanguage ==="js" ? "destructive" : "outline"} onClick={()=>{setSelectedLanguage("js")}}>Javascript</Button>
+              <Button variant={selectedLanguage ==="py" ? "destructive" : "outline"} onClick={()=>setSelectedLanguage("py")}>Python</Button>
+            
+              <Button onClick={async()=>{
+                  setStatus("Processing");
+                  setOutput("");
+                    
+                  const response = await axios.post(`${BACKEND_URL}/submit`, {
+                    "code": textRef.current?.value ,
+                    "language":selectedLanguage
+                  })
+    
+                  pollBackend(response.data.id);
+                
+              }} className="ml-30" >Submit</Button>
+          </div>
 
-        <span className="inline-flex items-center gap-1 select-none mr-5">
-            <span className="flex h-9 w-9 items-center justify-center rounded-lg bg-amber-500 text-xl font-black text-white">
-              U
-            </span>
+          <span className="inline-flex items-center gap-1 select-none mr-5">
+              <span className="flex h-9 w-9 items-center justify-center rounded-lg bg-amber-500 text-xl font-black text-white">
+                U
+              </span>
 
-            <span className="text-[23px] font-bold tracking-[-0.8px] text-slate-900">
-              Compile
-            </span>
-      </span>
-
+              <span className="text-[23px] font-bold tracking-[-0.8px] text-white">
+                Compile
+              </span>
+        </span>
       </div>
     
-      <div className="flex h-screen w-screen">
-        <div className="flex-1">
-          <textarea className="w-full h-full p-5 text-lg bg-zinc-500" ref={textRef}/>
-        </div>
 
-        <div className="bg-gray-700 flex-1 p-5 text-lg font-bold overflow-hidden">
-          <span  className= "flex text-gray-300">
-             Status <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 20 24" stroke-width="1.5" stroke="currentColor" className="size-8 mr-5 ml-3">
-                       <path stroke-linecap="round"  d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H5" />
-                    </svg>
-                    <span className={status=== "Failure" ? "text-red-300" : "text-green-500"}>
-                       {status}
-                    </span>
-          </span>
-          <div className="w-full h-full p-6 ">
-            <span className="text-gray-300">
-               Final Output :
+    
+      <div className="flex h-screen w-screen">
+        {/* left input area */}
+          <div className="flex-1">
+            <textarea className="w-full h-full p-5 text-lg bg-zinc-500 outline-none text-white font-normal" ref={textRef}/>
+          </div>
+
+
+          {/* right part  */}
+          <div className="bg-gray-700 flex-1 p-5  overflow-hidden ">
+
+            <span  className= "flex text-normal text-gray-200 pl-1">
+              Status :
+                  <span className={status=== "Failure" ? "text-red-300" : "text-green-500"}>
+                    {status}
+                  </span>
             </span>
-            <div  className={`p-2 border w-full border-3 rounded-lg border-stone-600 ${status === "Failure" ? "text-red-300" : "text-yellow-500"}`}>
-                  {output}
+
+             
+             <div className="mb-4 mt-2">
+                <span className="font-normal text-gray-200 text-base p-1"> If your code takes input, add it in the box below before running.</span>
+                  <div className="border border-gray-600 p-0 w-full h-35 font-normal text-white bg-gray-900">
+                      <textarea  className="w-full h-full p-3"></textarea>
+                  </div>
+             </div>
+
+            <div className="w-full h-full">
+                <span className="text-gray-200 text-base font-normal">
+                  Final Output :
+                </span>
+                <div  className={`p-2 border w-full border-2 bg-gray-900 border-gray-500 h-screen  mb-5 ${status === "Failure" ? "text-red-300" : "text-yellow-500"}`}>
+                      {output}
+                </div>
             </div>
           </div>
-        </div>
       </div>
+       <Footer/>
     </div>
   </> 
   )
---
+++ frontend/src/components/ui/footer.tsx
@@ -0,0 +1,18 @@
+export  function Footer() {
+    return <>
+     <div className="bg-gray-950 h-80 border-t-2 flex flex-col items-center justify-center gap-2"> 
+         <span className="inline-flex gap-1 select-none mr-5">
+              <span className="flex h-9 w-9 items-center justify-center rounded-lg bg-amber-500 text-xl font-black text-white">
+                U
+              </span>
+              <span className="text-[23px] font-bold tracking-[-0.8px] text-white">
+                Compile
+              </span>
+         </span>
+    <span className="text-normal text-gray-300"> 
+        “If it works, good. If it doesn’t, we don’t talk about it.”
+    </span>
+    </div>
+
+</>
+}
